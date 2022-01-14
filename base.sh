e154579a075f1febc80115a7e914815b950676d4
Soichi Hayashi <soichih@gmail.com>
2022-01-14 10:18:55 -0500

    I believe base.sh should be in a good shape now

--- base.sh
+++ base.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
 
+export OMP_NUM_THREADS=16
+
+rm -rf subjects
+mkdir -p subjects
+
 set -e
 set -x
 
-freesurfers=`jq -r .freesurfers config.json`
+#TODO - I can't use random index (like 0,1,2) to fake subject name. -long will look for the right id
+#so I need to find the data object ID and use that as subject name
 
+#construct recon-all commandline"
 cmd=`python << END
 cmd="-base template"
 import os
 import sys
 import json
+import hashlib
 with open("config.json") as f:
     config = json.load(f)
-for f in config["freesurfers"]:
 
-    #we are constructing command line argument! let's validate the user input
-    if os.path.isdir(f):
-        cmd+= " -tp "+f
-    else:
-        print("directory doesn't exist:"+f)
-        sys.exit(1)
+md5hash = hashlib.md5() 
+for i,f in enumerate(config["freesurfers"]):
+    #compute md5sum of norm.mgz to be used as tp name
+    with open(f+"/mri/norm.mgz") as fnorm:
+        norm = fnorm.read()
+        md5hash.update(norm)
+        digest = md5hash.hexdigest()
+    os.symlink("../"+f, "subjects/"+digest)
+    cmd+= " -tp "+digest
 print(cmd)
 END
 `
 
-export OMP_NUM_THREADS=8
-export SUBJECTS_DIR=`pwd`
-recon-all $cmd
+export SUBJECTS_DIR=`pwd`/subjects
+cd subjects
+recon-all $cmd -all
+
+cat > product.json <<EOF
+{
+    "meta":{
+        "longitudinal": true
+    },
+    "datatypes": [ "longitudinal" ]
+}
+EOF
+
--- long.sh
+++ long.sh
@@ -8,6 +8,14 @@ echo "running longitudinal processing"
 freesurfer=`jq -r .freesurfer config.json`
 template=`jq -r .template config.json`
 
-export OMP_NUM_THREADS=8
-export SUBJECTS_DIR=`pwd`
-recon-all -long $freesurfer $template -all
+#export OMP_NUM_THREADS=8
+
+rm -rf subjects
+mkdir -p subjects
+
+ln -s ../$freesurfer subjects/input
+ln -s ../$template subjects/template
+
+export SUBJECTS_DIR=`pwd`/subjects
+cd $SUBJECTS_DIR
+recon-all -long input template -all
